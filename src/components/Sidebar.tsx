184a88e21497d05432a4444dcb6e3b82ac23a462
Pranay Dutta <rick15176@gmail.com>
2025-04-02 21:43:16 +0530

    create sidebar: complete

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,48 @@
+import { Drawer, Heading, HStack, List, Portal, Text } from "@chakra-ui/react";
+import { FaBars } from "react-icons/fa";
+import Title from "./Title";
+import { items } from "./constants";
+
+const Sidebar = () => {
+  return (
+    <Drawer.Root size="xs">
+      <Drawer.Trigger asChild>
+        <FaBars size={24} />
+      </Drawer.Trigger>
+      <Portal>
+        <Drawer.Backdrop />
+        <Drawer.Positioner>
+          <Drawer.Content>
+            <Drawer.Header>
+              <Title />
+            </Drawer.Header>
+            <Drawer.Body>
+              <DrawerMenu />
+            </Drawer.Body>
+          </Drawer.Content>
+        </Drawer.Positioner>
+      </Portal>
+    </Drawer.Root>
+  );
+};
+const DrawerMenu = () => {
+  return (
+    <>
+      <Heading fontWeight="medium" fontSize="xl" mb={5}>
+        Menu
+      </Heading>
+      <List.Root listStyle="none" gap={4}>
+        {items.map((item) => (
+          <List.Item key={item.label}>
+            <HStack gap={4}>
+              <item.icon size={20} />
+              <Text fontSize="medium">{item.label}</Text>
+            </HStack>
+          </List.Item>
+        ))}
+      </List.Root>
+    </>
+  );
+};
+
+export default Sidebar;
