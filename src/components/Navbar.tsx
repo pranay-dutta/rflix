30ce71da7e0f77e2c8b1bfdab3fef04c91d9428a
Pranay Dutta <rick15176@gmail.com>
2025-04-02 20:46:38 +0530

    create sidebar: complete 60%

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,24 +8,30 @@ import {
   Text,
   List,
 } from "@chakra-ui/react";
+import { IconType } from "react-icons";
 import { BiSearch } from "react-icons/bi";
-import { FaBars } from "react-icons/fa";
+import { FaBars, FaDonate, FaHome } from "react-icons/fa";
+import { MdVideoLibrary } from "react-icons/md";
 import { RiMovie2AiFill } from "react-icons/ri";
 
-const Navbar = () => {
-  const items = ["Home", "Discover", "Donate"];
+const items: { icon: IconType; label: string }[] = [
+  { icon: FaHome, label: "Home" },
+  { icon: MdVideoLibrary, label: "Discover" },
+  { icon: FaDonate, label: "Donate" },
+];
 
+const Navbar = () => {
   return (
     <HStack px={5} py={2} justifyContent="space-between">
       <Box display="flex" alignItems="center" gap={2}>
         <RiMovie2AiFill color="aqua" size={24} />
-        <Text>Nunflix</Text>
+        <Text fontSize="xl" fontWeight="bold">Nunflix</Text>
       </Box>
 
       <Box mx="auto" display="none" md={{ display: "block" }}>
         <SegmentGroup.Root defaultValue="Home">
           <SegmentGroup.Indicator />
-          <SegmentGroup.Items items={items} />
+          <SegmentGroup.Items items={items.map((item) => item.label)} />
         </SegmentGroup.Root>
       </Box>
 
@@ -43,7 +49,6 @@ const Navbar = () => {
 export default Navbar;
 
 const Demo = () => {
-  const items = ["Home", "Discover", "Donate"];
   return (
     <Drawer.Root size="xs">
       <Drawer.Trigger asChild>
@@ -56,15 +61,20 @@ const Demo = () => {
           <Drawer.Content>
             <Drawer.Header>
               <Box display="flex" alignItems="center" gap={2}>
-                <RiMovie2AiFill color="aqua" size={24} />
-                <Text>Nunflix</Text>
+                <RiMovie2AiFill color="aqua" size={40} />
+                <Text fontSize="2xl">Nunflix</Text>
               </Box>
             </Drawer.Header>
             <Drawer.Body>
-              <Text>Menu</Text>
-              <List.Root>
+              <Text fontWeight="medium" fontSize="xl" mt={3} mb={5}>Menu</Text>
+              <List.Root listStyle="none" gap={3}>
                 {items.map((item) => (
-                  <List.Item key={item}>{item}</List.Item>
+                  <List.Item key={item.label}>
+                    <HStack gap={4}>
+                      <item.icon size={18} />
+                      {item.label}
+                    </HStack>
+                  </List.Item>
                 ))}
               </List.Root>
             </Drawer.Body>
