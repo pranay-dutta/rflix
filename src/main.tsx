4cd7dac498469726304bec69321a1f493253025c
Pranay Dutta <rick15176@gmail.com>
2025-04-08 03:51:33 +0530

    use swiper

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
+import "swiper/swiper-bundle.css";
 import "./index.css";
 import router from "./routes";
 
