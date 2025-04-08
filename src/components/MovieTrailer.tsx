fa3c6ecf5322525da2676433f0ed64ff23e051f3
Pranay Dutta <rick15176@gmail.com>
2025-04-08 21:19:32 +0530

    create movie trailer component

---
+++ src/components/MovieTrailer.tsx
@@ -0,0 +1,46 @@
+import useMovie from "@/hooks/useMovie";
+import useTrailers from "@/hooks/useTrailers";
+import { useEffect, useRef } from "react";
+
+interface Props {
+  isActive: boolean;
+  movieId: number;
+}
+
+const MovieTrailer = ({ movieId, isActive = false }: Props) => {
+  const { trailers } = useTrailers(movieId);
+  const { movie } = useMovie(movieId);
+  const iframeRef = useRef<HTMLIFrameElement>(null);
+
+  // ▶️ Play or ⏸ Pause depending on isActive
+  useEffect(() => {
+    if (!iframeRef.current) return;
+
+    iframeRef.current.contentWindow?.postMessage(
+      JSON.stringify({
+        event: "command",
+        func: isActive ? "playVideo" : "pauseVideo",
+        args: [],
+      }),
+      "*"
+    );
+  }, [isActive]);
+
+  const youtubeId = trailers?.find(
+    (trailer) => trailer.type === "Trailer"
+  )?.key;
+  if (!youtubeId) return null;
+
+  return (
+    <iframe
+      ref={iframeRef}
+      src={`https://www.youtube.com/embed/${youtubeId}?enablejsapi=1&mute=1&autoplay=1&loop=1&rel=0&fs=0&controls=0&disablekb=1&playlist=${youtubeId}`}
+      style={{ scale: 1.5 }}
+      title={movie?.original_title}
+      allowFullScreen
+      loading="lazy"
+    />
+  );
+};
+
+export default MovieTrailer;
