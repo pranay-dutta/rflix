3788ec918c612900a4385c3b54e03ef13f5a385b
Pranay Dutta <rick15176@gmail.com>
2025-04-09 23:25:23 +0530

    Build hero component

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -9,16 +9,20 @@ import MovieOverview from "./MovieOverview";
 import MovieTrailer from "./MovieTrailer";
 import Rating from "./Rating";
 import ReleaseDate from "./ReleaseDate";
+import Button from "./Button";
+import { FaInfoCircle } from "react-icons/fa";
+import { FaPlay } from "react-icons/fa6";
 
 const Hero2 = () => {
-  const { data: movies } = useMovies(1);
+  const { movies } = useMovies(1, "upcoming");
   const [activeIndex, setActiveIndex] = useState<number>(0);
 
   if (!movies) return null;
+  console.log(movies);
 
   return (
     <Swiper onSlideChange={(swiper) => setActiveIndex(swiper.activeIndex)}>
-      {movies.results.map((movie, index) => {
+      {movies.map((movie, index) => {
         return (
           <>
             <div className="relative">
@@ -30,15 +34,23 @@ const Hero2 = () => {
                   />
                 </AspectRatioContainer>
 
-                {/* Movie Informations */}
-                <Box className="w-full flex flex-col gap-3 items-center md:items-start absolute bottom-5 left-0 md:bottom-[20%] md:left-[2%]">
-                  <Box p={5}><MovieLogo movieId={movie.id} /></Box>
+                <Box className="w-full flex flex-col gap-3 items-center md:items-start absolute bottom-5 left-0 md:bottom-[10%] md:left-[2%]">
+                  {/* Movie title logo */}
+                  <Box p={5} maxH={500} maxW={500}>
+                    <MovieLogo movieId={movie.id} />
+                  </Box>
+
+                  {/* Movie Informations */}
                   <HStack gap={4}>
                     <Rating rating={movie.vote_average} />
                     <ReleaseDate date={movie.release_date} />
                     <MovieLanguage language={movie.original_language} />
                   </HStack>
                   <MovieOverview overview={movie.overview} />
+                  <HStack gap={5}>
+                    <Button icon={FaInfoCircle}>More info</Button>
+                    <Button icon={FaPlay}>Play</Button>
+                  </HStack>
                 </Box>
               </SwiperSlide>
             </div>
