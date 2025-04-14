068f71064d81eb6d35c99925d9436ac02b6cc109
Pranay Dutta <rick15176@gmail.com>
2025-04-14 21:19:19 +0530

    add card placeholder and card hover description

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -1,54 +1,74 @@
-import { Box, HStack, Image, Text } from "@chakra-ui/react";
-import { getTMDBImage } from "./constants";
 import { Movie } from "@/hooks/useMovies";
-import Rating from "./Rating";
+import { Box, HStack, Image, Text, Stack } from "@chakra-ui/react";
 import { useState } from "react";
+import { getPlaceHolder, getTMDBImage } from "./constants";
+import Rating from "./Rating";
 import ReleaseDate from "./ReleaseDate";
+import { Link } from "react-router-dom";
 
 const MovieCard = ({ movie }: { movie: Movie }) => {
-  const [showRating, setShowRating] = useState<boolean>(false);
-  return (
-    <Box
-      className="cursor-pointer relative"
-      py={3}
-      _hover={{
-        scale: 1.03,
-        transition: "all 0.3s ease-in-out",
-        transform: "translateY(-5px)",
-      }}
-      transition="all 0.3s ease-in-out"
-      onMouseEnter={() => setShowRating(true)}
-      onMouseLeave={() => setShowRating(false)}
-    >
-      {/* Image of movie card */}
-      <Image
-        borderRadius="md"
-        _hover={{
-          filter: "brightness(0.6)",
-          transition: "all 0.3s ease-in-out",
-        }}
-        src={getTMDBImage(movie.poster_path, "w500")}
-        alt={movie.original_title}
-        objectFit="cover"
-      />
-      <Text mt={2} lineClamp={1}>
-        {movie.title}
-      </Text>
+  const [show, setShow] = useState<boolean>(false);
 
-      {/* Movie card rating and release date*/}
-      {showRating && (
-        <HStack
-          position="absolute"
-          top={4}
-          px={2}
-          w="full"
-          justify="space-between"
-        >
-          <ReleaseDate date={movie.release_date} />
-          <Rating rating={movie.vote_average} />
-        </HStack>
-      )}
-    </Box>
+  return (
+    <Link to={"/info/" + movie.id}>
+      <Box
+        className="cursor-pointer relative"
+        py={3}
+        _hover={{
+          scale: 1.03,
+          transition: "all 0.3s ease-in-out",
+          transform: "translateY(-5px)",
+        }}
+        transition="all 0.3s ease-in-out"
+        onMouseEnter={() => setShow(true)}
+        onMouseLeave={() => setShow(false)}
+      >
+        {/* Image of movie card */}
+        <Box position="relative">
+          <Image
+            borderRadius="md"
+            filter={show ? "brightness(0.8)" : "brightness(1)"}
+            src={
+              movie.poster_path
+                ? getTMDBImage(movie.poster_path, "w500")
+                : getPlaceHolder()
+            }
+            alt={movie.original_title}
+            objectFit="cover"
+          />
+          {
+            /* Bottom gradient */
+            show && (
+              <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black/90" />
+            )
+          }
+        </Box>
+
+        {/* Movie card rating and release date*/}
+        {show && (
+          <>
+            <HStack
+              position="absolute"
+              top={4}
+              px={2}
+              w="full"
+              justify="space-between"
+            >
+              <ReleaseDate date={movie.release_date} />
+              <Rating rating={movie.vote_average} />
+            </HStack>
+            <Stack position="absolute" bottom={10} px={2} w="full" gap={2}>
+              <Text fontSize="md" fontWeight="medium" lineClamp="1">
+                {movie.title}
+              </Text>
+              <Text fontSize="sm" lineClamp="2" lineHeight="1">
+                {movie.overview}
+              </Text>
+            </Stack>
+          </>
+        )}
+      </Box>
+    </Link>
   );
 };
 
