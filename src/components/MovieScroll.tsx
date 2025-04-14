9ba47579462c7a419c7d085dd36291739a86051d
Pranay Dutta <rick15176@gmail.com>
2025-04-14 21:20:42 +0530

    add dynamic breakpoints in MovieScroll component

--- src/components/MovieScroll.tsx
+++ src/components/MovieScroll.tsx
@@ -5,8 +5,32 @@ import MovieCard from "./MovieCard";
 const MovieScroll = ({ movies }: { movies: Movie[] }) => {
   return (
     <Swiper
-      slidesPerView={8}
-      spaceBetween={10}
+      breakpoints={{
+        200: {
+          slidesPerView: 2,
+          spaceBetween: 15,
+        },
+        640: {
+          slidesPerView: 3,
+          spaceBetween: 15,
+        },
+        768: {
+          slidesPerView: 5,
+          spaceBetween: 10,
+        },
+        1024: {
+          slidesPerView: 6,
+          spaceBetween: 15,
+        },
+        1080: {
+          slidesPerView: 7,
+          spaceBetween: 15,
+        },
+        1536: {
+          slidesPerView: 8,
+          spaceBetween: 15,
+        },
+      }}
       loop
     >
       {movies.map((movie) => (
