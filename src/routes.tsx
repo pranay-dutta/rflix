6050393f2bb3065b147b677373aa7333890ed9e2
Pranay Dutta <rick15176@gmail.com>
2025-04-18 03:02:15 +0530

    update routes: add tv info route

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,21 +2,40 @@ import { createBrowserRouter } from "react-router-dom";
 import Layout from "./pages/Layout";
 import HomePage from "./pages/HomePage";
 import WatchPage from "./pages/WatchPage";
+import InfoPage from "./pages/InfoPage";
+import InfoPageCopy from "./pages/InfoPageCopy";
+import SearchPage from "./pages/SearchPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <HomePage />,
-    // children: [
-    //   {
-    //     index: true,
-    //     // element: <HomePage />,
-    //   },
-    //   {
-    //     path: "/watch/:id",
-    //     element: <WatchPage />,
-    //   },
-    // ],
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: "/info/movie/:id",
+        element: <InfoPage />,
+      },
+      {
+        path: "/info/tv/:id",
+        element: <h1>Tv info page</h1>,
+      },
+      {
+        path: "/watch/:id",
+        element: <WatchPage />,
+      },
+      {
+        path: "/search",
+        element: <SearchPage />,
+      },
+      {
+        path: "/info-copy/:id",
+        element: <InfoPageCopy />,
+      },
+    ],
   },
 ]);
 export default router;
