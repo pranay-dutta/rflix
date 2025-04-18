29837afb20ce212880e9318d18da09685b96d3d6
Pranay Dutta <rick15176@gmail.com>
2025-04-18 12:16:15 +0530

    build tabs for movie and tv series

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,75 +1,38 @@
-import MovieCard from "@/components/MovieCard";
-import { Movie } from "@/hooks/useMovies";
-import axiosInstance from "@/services/api-client";
-import { Container, SimpleGrid, Spinner } from "@chakra-ui/react";
-import { useInfiniteQuery } from "@tanstack/react-query";
-import { useSearchParams } from "react-router-dom";
-import InfiniteScroll from "react-infinite-scroll-component";
-import { Fragment } from "react/jsx-runtime";
-
-interface FetchResponse {
-  results: Movie[];
-}
+import MovieGrid from "@/components/MovieGrid";
+import TvShowsGrid from "@/components/TvSeriesGrid";
+import { Container } from "@chakra-ui/react";
+import { Tabs } from "@chakra-ui/react";
+import { FaTv } from "react-icons/fa";
+import { MdMovie } from "react-icons/md";
 
 const SearchPage = () => {
-  const [searchParams] = useSearchParams();
-  const query = searchParams.get("query") || "";
-  const {
-    data,
-    isFetchingNextPage,
-    hasNextPage,
-    fetchNextPage,
-    isLoading,
-    error,
-  } = useInfiniteQuery<FetchResponse, Error>({
-    queryKey: ["search", query],
-    initialPageParam: 1,
-    queryFn: ({ pageParam = 1 }) =>
-      axiosInstance
-        .get<FetchResponse>("/search/movie?", {
-          params: { query: query, page: pageParam },
-        })
-        .then((res) => res.data),
-    getNextPageParam: (lastPage, pages) => {
-      if (lastPage.results.length === 0) return undefined;
-      return pages.length + 1;
-    },
-  });
-
-  if (isLoading) return <div>Loading...</div>;
-  if (error) return <div>Error: {error.message}</div>;
-
-  const resCount =
-    data?.pages.reduce((acc, page) => acc + page.results.length, 0) || 0;
-
   return (
-    <Container>
-      <InfiniteScroll
-        dataLength={resCount}
-        hasMore={hasNextPage}
-        next={fetchNextPage}
-        loader={isFetchingNextPage && <Spinner my={3} size="md" />}
-        style={{ overflow: "unset" }} // Important! To prevent scroll jump
-      >
-        <SimpleGrid
-          gap={4}
-          columns={{
-            base: 2,
-            md: 3,
-            lg: 4,
-            xl: 5,
-          }}
-          // className="grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4"
+    <Container py={10}>
+      <Tabs.Root defaultValue="movies" variant="plain" size="lg">
+        <Tabs.List
+          bg="bg.muted"
+          rounded="l3"
+          p="1"
+          width="full"
+          justifyContent="space-around"
         >
-          {data?.pages.map((page, index) => (
-            <Fragment key={index}>
-              {page.results.map((movie) => (
-                <MovieCard key={movie.id} movie={movie} />
-              ))}
-            </Fragment>
-          ))}
-        </SimpleGrid>
-      </InfiniteScroll>
+          <Tabs.Trigger value="movies" w={"50%"}>
+            <MdMovie />
+            Movies
+          </Tabs.Trigger>
+          <Tabs.Trigger value="tvshows" w={"50%"}>
+            <FaTv />
+            Tv Shows
+          </Tabs.Trigger>
+          <Tabs.Indicator rounded="l2" />
+        </Tabs.List>
+        <Tabs.Content value="movies">
+          <MovieGrid />
+        </Tabs.Content>
+        <Tabs.Content value="tvshows">
+          <TvShowsGrid />
+        </Tabs.Content>
+      </Tabs.Root>
     </Container>
   );
 };
