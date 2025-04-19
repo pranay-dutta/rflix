9d94e405eb2bb40ad8e137997e2f33e83f88a393
Pranay Dutta <rick15176@gmail.com>
2025-04-19 19:30:04 +0530

    create a footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,16 @@
+import { Text } from "@chakra-ui/react";
+
+const Footer = () => {
+  const date = new Date();
+  return (
+    <footer className="py-3 text-center text-gray-400">
+      <Text>
+        This site does not store any files on it's server, we only link to the
+        media which is hosted on 3rd party services.
+      </Text>
+      <Text>&copy; {date.getFullYear()} Rflix. All rights reserved. </Text>
+    </footer>
+  );
+};
+
+export default Footer;
