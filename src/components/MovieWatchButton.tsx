5184d5d41e6a46682c8a0d46be4ec20ea9852c18
Pranay Dutta <rick15176@gmail.com>
2025-04-20 00:57:39 +0530

    separate buttons for movie watch and tvseries watch

---
+++ src/components/MovieWatchButton.tsx
@@ -0,0 +1,37 @@
+import { Button as ChakraButton } from "@chakra-ui/react";
+import { ReactNode } from "react";
+import { IconType } from "react-icons";
+import { useNavigate } from "react-router-dom";
+
+interface Props {
+  children: ReactNode;
+  icon: IconType;
+  id: number;
+}
+
+const WatchButton = ({ children, icon, id }: Props) => {
+  const Icon = icon;
+  const navigate = useNavigate();
+
+  return (
+    <ChakraButton
+      fontSize="sm"
+      paddingX={5}
+      paddingY={2}
+      backgroundColor="whiteAlpha.200"
+      color="whiteAlpha.900"
+      borderRadius="md"
+      fontWeight={500}
+      _focus={{ outline: "none", boxShadow: "outline" }}
+      rounded="md"
+      _hover={{ scale: 1.1, opacity: 0.9 }}
+      width="fit-content"
+      onClick={() => navigate("/watch/movie/" + id)}
+    >
+      <Icon />
+      {children}
+    </ChakraButton>
+  );
+};
+
+export default WatchButton;
