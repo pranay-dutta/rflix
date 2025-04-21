d314ffd3c6efb257cf9e25e9bc6e42f16d8d2e73
Pranay Dutta <rick15176@gmail.com>
2025-04-21 21:32:12 +0530

    refactor: make a class of ApiClient

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,12 +1,30 @@
-import axios from "axios";
-const { VITE_TMDB_ACCESS_AUTH_TOKEN } = import.meta.env;
+import { FetchResponse } from "@/interfaces/FetchResponse";
+import axios, { AxiosRequestConfig } from "axios";
+const { VITE_TMDB_API_KEY } = import.meta.env;
 
 const axiosInstance = axios.create({
   baseURL: "https://api.themoviedb.org/3",
+  params: { api_key: VITE_TMDB_API_KEY },
   headers: {
     Accept: "application/json",
-    Authorization: "Bearer " + VITE_TMDB_ACCESS_AUTH_TOKEN,
   },
 });
 
+class ApiClient<T> {
+  endpoint: string;
+
+  constructor(endpoint: string) {
+    this.endpoint = endpoint;
+  }
+  get = async () => {
+    return axiosInstance.get<T>(this.endpoint).then((res) => res.data);
+  };
+  getAll = async (params?: AxiosRequestConfig) => {
+    return axiosInstance
+      .get<FetchResponse<T>>(this.endpoint, params)
+      .then((res) => res.data);
+  };
+}
+
 export default axiosInstance;
+export { ApiClient };
