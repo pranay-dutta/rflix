7d21bd2668f3fb631b3eedb5b3fb6537d77affcd
Pranay Dutta <rick15176@gmail.com>
2025-05-29 01:46:02 +0530

    refactor: add skeleton & fix hide similar movie heading no similar movie

--- src/pages/MovieInfoPage.tsx
+++ src/pages/MovieInfoPage.tsx
@@ -9,6 +9,7 @@ import {
   HStack,
   Image,
   SimpleGrid,
+  Skeleton,
   Stack,
   Text,
 } from "@chakra-ui/react";
@@ -16,132 +17,125 @@ import { useParams } from "react-router-dom";
 import ReleaseDate from "@/components/ReleaseDate";
 import Rating from "@/components/Rating";
 import useSimilarMovies from "@/hooks/useSimilarMovies";
-import Gradient from "@/components/Gradient";
 import { Quote } from "../components/Quote";
 import Runtime from "@/components/Runtime";
 import BackButton from "@/components/BackButton";
 import MovieWatchButton from "@/components/WatchButton";
-import { MediaScroll, MediaScrollHeading } from "@/components/common";
-import { Movie } from "@/interfaces/Movie";
+import { MediaScroll, MediaScrollHeading, MediaPoster } from "@/components/common";
+import { MovieDetails } from "@/interfaces/MovieDetails";
 
 const MovieInfoPage = () => {
   const { id } = useParams();
   if (!id) throw new Error("Info page");
 
-  const { similarMovies } = useSimilarMovies(1, parseInt(id));
-  const { movie } = useMovie(parseInt(id));
-
-  if (!movie || !similarMovies) return null;
+  const { movie, isLoading } = useMovie(parseInt(id));
 
   return (
     <>
       <Box my={3}>
         <BackButton />
       </Box>
-      <Box position="relative" height="fit-content">
-        <MediaPoster media={movie} />
-        <Box display={{ base: "none", md: "block" }}>
-          <Gradient.Bottom />
-        </Box>
-
-        <SimpleGrid
-          className="md:absolute left-[2%] md:top-2 md:bottom-auto lg:top-auto lg:bottom-[3%]"
-          columns={{ base: 1, md: 4 }}
-          gap={{ sm: 2, lg: 5 }}
-        >
-          {/* Card image above poster */}
-          <GridItem>
-            <Image
-              src={getTMDBImage(movie.poster_path, "original", "vertical")}
-              borderRadius="md"
-            />
-          </GridItem>
-          <GridItem colSpan={{ md: 3 }} px={2}>
-            <Box display="flex" flexDirection="column" gap={5}>
-              <Stack gap={4}>
-                <Heading fontSize="4xl">{movie.title}</Heading>
-                <Quote tagline={movie.tagline} />
-              </Stack>
-              <HStack flexWrap="wrap">
-                {movie.genres.map((genre) => (
-                  <Badge size="md" key={genre.id}>
-                    {genre.name}
-                  </Badge>
-                ))}
-              </HStack>
-              <HStack>
-                <ReleaseDate date={movie.release_date} />
-                <Runtime runtime={movie.runtime} />
-                <Rating rating={movie.vote_average} />
-              </HStack>
-              <Text lineClamp={2} fontSize={{ sm: "sm", md: "normal" }}>
-                {movie.overview}
-              </Text>
-              <MovieWatchButton id={movie.id} icon={BiPlay}>
-                Watch Now
-              </MovieWatchButton>
-            </Box>
-
-            <Stack mt={5}>
-              <HStack
-                flexDirection={{ base: "column", md: "row" }}
-                alignItems={{ base: "flex-start", md: "center" }}
-                maxW={{ md: "3/4" }}
-                justify="space-between"
-              >
-                <Box>
-                  <Text fontWeight="medium">Production</Text>
-                  <HStack>
-                    {movie.production_companies.map((company, index) =>
-                      index <= 2 ? (
-                        <Text key={company.id} fontSize="sm" fontStyle="italic" color="gray.300">
-                          {company.name}
-                          {index + 1 != movie.production_companies.length && <span>{" •"}</span>}
-                        </Text>
-                      ) : null,
-                    )}
-                  </HStack>
-                </Box>
-
-                <Box>
-                  <Text fontWeight="medium">Language</Text>
-                  <Text fontSize="sm" fontStyle="italic" color="gray.300">
-                    {movie.original_language.toUpperCase()}
-                  </Text>
-                </Box>
-              </HStack>
-            </Stack>
-          </GridItem>
-        </SimpleGrid>
-      </Box>
-
-      {/* Similar movies scroll compoent */}
-      <SimilarMovies movies={similarMovies} />
+      {isLoading ? <Skeleton w="full" h="774px" /> : <MovieHero movie={movie} />}
     </>
   );
 };
 
-const SimilarMovies = ({ movies }: { movies: Movie[] }) => {
-  if (!movies.length) return null;
+const SimilarMovies = () => {
+  const { id } = useParams();
+  if (!id) throw new Error("Info page");
+
+  const { similarMovies } = useSimilarMovies(1, parseInt(id));
+  if (!similarMovies || !similarMovies.length) return null;
+
   return (
     <Box mt={10}>
       <MediaScrollHeading highlight="Movies">Similar Movies</MediaScrollHeading>
-      <MediaScroll media={movies} />
+      <MediaScroll media={similarMovies} />
     </Box>
   );
 };
 
 export default MovieInfoPage;
 
-const MediaPoster = ({ media }: { media: Movie }) => {
-  if (!media) return null;
-  return (
-    <Box className="opacity-50 hidden! md:block! rounded-lg">
-      <Image
-        className="opacity-70 w-full rounded-md object-cover"
-        src={getTMDBImage(media.backdrop_path, "original", "horizontal")}
-        alt={media.original_title}
-      />
+const MovieHero = ({ movie }: { movie: MovieDetails | undefined }) => {
+  if (!movie) return null;
+
+  return <>
+    <Box position="relative" height="fit-content">
+      <MediaPoster backdrop_path={movie.backdrop_path} />
+
+      <SimpleGrid
+        className="md:absolute left-[2%] md:top-2 md:bottom-auto lg:top-auto lg:bottom-[3%]"
+        columns={{ base: 1, md: 4 }}
+        gap={{ sm: 2, lg: 5 }}
+      >
+        {/* Card image above poster */}
+        <GridItem>
+          <Image
+            src={getTMDBImage(movie.poster_path, "original", "vertical")}
+            borderRadius="md"
+          />
+        </GridItem>
+        <GridItem colSpan={{ md: 3 }} px={2}>
+          <Box display="flex" flexDirection="column" gap={5}>
+            <Stack gap={4}>
+              <Heading fontSize="4xl">{movie.title}</Heading>
+              <Quote tagline={movie.tagline} />
+            </Stack>
+            <HStack flexWrap="wrap">
+              {movie.genres.map((genre) => (
+                <Badge size="md" key={genre.id}>
+                  {genre.name}
+                </Badge>
+              ))}
+            </HStack>
+            <HStack>
+              <ReleaseDate date={movie.release_date} />
+              <Runtime runtime={movie.runtime} />
+              <Rating rating={movie.vote_average} />
+            </HStack>
+            <Text lineClamp={2} fontSize={{ sm: "sm", md: "normal" }}>
+              {movie.overview}
+            </Text>
+            <MovieWatchButton id={movie.id} icon={BiPlay}>
+              Watch Now
+            </MovieWatchButton>
+          </Box>
+
+          <Stack mt={5}>
+            <HStack
+              flexDirection={{ base: "column", md: "row" }}
+              alignItems={{ base: "flex-start", md: "center" }}
+              maxW={{ md: "3/4" }}
+              justify="space-between"
+            >
+              <Box>
+                <Text fontWeight="medium">Production</Text>
+                <HStack>
+                  {movie.production_companies.map((company, index) =>
+                    index <= 2 ? (
+                      <Text key={company.id} fontSize="sm" fontStyle="italic" color="gray.300">
+                        {company.name}
+                        {index + 1 != movie.production_companies.length && <span>{" •"}</span>}
+                      </Text>
+                    ) : null,
+                  )}
+                </HStack>
+              </Box>
+
+              <Box>
+                <Text fontWeight="medium">Language</Text>
+                <Text fontSize="sm" fontStyle="italic" color="gray.300">
+                  {movie.original_language.toUpperCase()}
+                </Text>
+              </Box>
+            </HStack>
+          </Stack>
+        </GridItem>
+      </SimpleGrid>
     </Box>
-  );
-};
+
+    {/* Similar movies scroll compoent */}
+    <SimilarMovies />
+  </>
+}
