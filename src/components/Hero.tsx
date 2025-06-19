db6a6a1352e886282860f32a98f96a9319699eed
Pranay Dutta <rick15176@gmail.com>
2025-06-20 02:26:04 +0530

    refactor: hero video cursor feedback for better ux

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -15,6 +15,7 @@ import InfoButton from "./InfoButton";
 import MovieWatchButton from "./WatchButton";
 import useMovieLists from "@/hooks/useMovieLists";
 
+
 const Hero = () => {
   const { data: movies } = useMovieLists("popular");
   const [activeIndex, setActiveIndex] = useState<number>(0);
@@ -26,48 +27,46 @@ const Hero = () => {
       onSlideChange={(swiper) => setActiveIndex(swiper.activeIndex)}
       autoplay={{ delay: 30 * 1000 }}
       modules={[Autoplay]}
+      grabCursor
     >
       {movies.pages[0].results.map((movie, index) => {
         return (
-          <>
-            <div className="relative">
-              <SwiperSlide key={movie.id} className="transition-all relative">
-                <AspectRatioContainer>
-                  <MovieTrailer movieId={movie.id} isActive={activeIndex === index} />
-                </AspectRatioContainer>
+          <div className="relative">
+            <SwiperSlide key={movie.id} className="transition-all relative">
+              <AspectRatioContainer>
+                <MovieTrailer movieId={movie.id} isActive={activeIndex === index} />
+              </AspectRatioContainer>
 
-                {/* Bottom shadow over hero component */}
-                <div className="absolute h-40 top-10/12 inset-0 bg-gradient-to-b from-transparent via-transparent to-black/100" />
-                <Box
-                  fontSize="lg"
-                  className="w-full !px-5 flex flex-col gap-3 items-center md:items-start absolute bottom-5 left-0 md:bottom-[20%] md:left-[1%]"
-                >
-                  {/* Movie title logo */}
-                  <Box maxH={300} maxW={350} >
-                    <MovieLogo movieId={movie.id} />
-                  </Box>
-
-                  {/* Movie Informations */}
-                  <HStack filter="contrast(2)" gap={4}>
-                    <Rating rating={movie.vote_average} />
-                    <ReleaseDate date={movie.release_date} />
-                    <MovieLanguage language={movie.original_language} />
-                  </HStack>
-                  <MovieOverview overview={movie.overview} />
-
-                  <HStack gap={5}>
-                    <InfoButton icon={FaInfoCircle} id={movie.id}>
-                      More info
-                    </InfoButton>
-                    <MovieWatchButton id={movie.id} icon={FaPlay}>
-                      Play
-                    </MovieWatchButton>
-                  </HStack>
+              {/* Bottom shadow over hero component */}
+              <div className="absolute h-40 top-10/12 inset-0 bg-gradient-to-b from-transparent via-transparent to-black/100" />
+              <Box
+                fontSize="lg"
+                className="w-full !px-5 flex flex-col gap-3 items-center md:items-start absolute bottom-5 left-0 md:bottom-[20%] md:left-[1%]"
+              >
+                {/* Movie title logo */}
+                <Box maxH={300} maxW={350} >
+                  <MovieLogo movieId={movie.id} />
                 </Box>
-              </SwiperSlide>
 
-            </div>
-          </>
+                {/* Movie Informations */}
+                <HStack filter="contrast(2)" gap={4}>
+                  <Rating rating={movie.vote_average} />
+                  <ReleaseDate date={movie.release_date} />
+                  <MovieLanguage language={movie.original_language} />
+                </HStack>
+                <MovieOverview overview={movie.overview} />
+
+                <HStack gap={5}>
+                  <InfoButton icon={FaInfoCircle} id={movie.id}>
+                    More info
+                  </InfoButton>
+                  <MovieWatchButton id={movie.id} icon={FaPlay}>
+                    Play
+                  </MovieWatchButton>
+                </HStack>
+              </Box>
+            </SwiperSlide>
+          </div>
         );
       })}
 
