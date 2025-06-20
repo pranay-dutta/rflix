a005d56b88ba73c1584751672c3e740d0a05b3dd
Pranay Dutta <rick15176@gmail.com>
2025-06-20 16:14:47 +0530

    refactor: generic sort dropdown component

--- src/components/common/SortDropDown.tsx
+++ src/components/common/SortDropDown.tsx
@@ -2,18 +2,17 @@ import { createListCollection, Portal, Select } from "@chakra-ui/react";
 import { useSearchParams } from "react-router-dom";
 import { Fragment } from "react/jsx-runtime";
 
-const SortDropDown = () => {
-  return <div >
-    <DropDown />
-  </div>
+const SortDropDown = ({ mediaType }: { mediaType: "movie" | "tv" }) => {
+  return <DropDown mediaType={mediaType} />
 }
 
-const DropDown = () => {
+const DropDown = ({ mediaType }: { mediaType: "movie" | "tv" }) => {
   const [, setSearchParams] = useSearchParams();
+  const collections = mediaType === "movie" ? movieSortCollections : tvSortCollections;
 
   return (
     <>
-      <Select.Root collection={sortCollections} minW={{ base: "100%", md: "250px" }}>
+      <Select.Root collection={collections} minW={{ base: "100%", md: "250px" }}>
         <Select.HiddenSelect />
         <Select.Control>
           <Select.Trigger>
@@ -27,14 +26,13 @@ const DropDown = () => {
         <Portal>
           <Select.Positioner>
             <Select.Content>
-              {sortCollections.items.map((sortItem, i) => (
-                <Fragment key={i} >
-                  <Select.Item item={sortItem} key={sortItem.value} onClick={() => {
-                    setSearchParams((searchParams) => {
-                      searchParams.set("sort_by", sortItem.value)
-                      return searchParams;
-                    })
-                  }}>{sortItem.label}
+              {collections.items.map((sortItem) => (
+                <Fragment key={sortItem.value} >
+                  <Select.Item
+                    item={sortItem}
+                    onClick={() => setSearchParams({ "sort_by": sortItem.value })}
+                  >
+                    {sortItem.label}
                     <Select.ItemIndicator />
                   </Select.Item>
                 </Fragment>
@@ -47,19 +45,36 @@ const DropDown = () => {
   );
 };
 
-const sortCollections = createListCollection({
+const movieSortCollections = createListCollection({
   items: [
     { label: "Popularity Ascending", value: "popularity.asc" },
     { label: "Popularity Descending", value: "popularity.desc" },
-
-    { label: "Rating Ascending", value: "vote_count.asc" },
-    { label: "Rating Descending", value: "vote_count.desc" },
-
-    { label: "First Air Date Ascending", value: "primary_release_date.asc" },
-    { label: "First Air Date Descending", value: "primary_release_date.desc" },
-
+    { label: "Vote Ascending", value: "vote_count.asc" },
+    { label: "Vote Descending", value: "vote_count.desc" },
+    { label: "Rating Ascending", value: "vote_average.asc" },
+    { label: "Rating Descending", value: "vote_average.desc" },
+    { label: "Release Date Ascending", value: "primary_release_date.asc" },
+    { label: "Release Date Descending", value: "primary_release_date.desc" },
     { label: "Name (A-Z)", value: "original_title.asc" },
     { label: "Name (Z-A)", value: "original_title.desc" },
-  ]
-})
+  ],
+});
+
+const tvSortCollections = createListCollection({
+  items: [
+    { label: "Popularity Ascending", value: "popularity.asc" },
+    { label: "Popularity Descending", value: "popularity.desc" },
+    { label: "Vote Ascending", value: "vote_count.asc" },
+    { label: "Vote Descending", value: "vote_count.desc" },
+    { label: "Rating Ascending", value: "vote_average.asc" },
+    { label: "Rating Descending", value: "vote_average.desc" },
+    { label: "First Air Date Ascending", value: "first_air_date.asc" },
+    { label: "First Air Date Descending", value: "first_air_date.desc" },
+    { label: "Name (A-Z)", value: "original_name.asc" },
+    { label: "Name (Z-A)", value: "original_name.desc" },
+  ],
+});
+
+
+
 export default SortDropDown
