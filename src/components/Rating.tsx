c73775457768ebb03ebed0502ef3c4abb365a2f7
Pranay Dutta <rick15176@gmail.com>
2025-07-12 22:08:55 +0530

    fix: discover page crash

--- src/components/Rating.tsx
+++ src/components/Rating.tsx
@@ -2,7 +2,8 @@ import { HStack } from "@chakra-ui/react";
 import { FaStar } from "react-icons/fa";
 
 const Rating = ({ vote_average }: { vote_average: number }) => {
-  if (vote_average < 1) return null;
+  if (!vote_average) return null;
+
   return (
     <HStack gap={1}>
       <FaStar color="orange" />
