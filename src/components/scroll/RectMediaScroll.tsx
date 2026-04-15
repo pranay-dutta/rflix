d88ac8b4c361013d3610321fa76cb04410da3fb7
Pranay Dutta <rick15176@gmail.com>
2026-04-16 02:55:52 +0530

    feat: implement custom arrows to navigate and fractional number of slides

--- src/components/scroll/RectMediaScroll.tsx
+++ src/components/scroll/RectMediaScroll.tsx
@@ -1,6 +1,10 @@
 import { Movie } from "@/interfaces/Movie";
 import TvSeries from "@/interfaces/TvSeries";
 import { Box, Skeleton } from "@chakra-ui/react";
+import { useRef } from "react";
+import {} from "react-icons";
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa6";
+import { Navigation } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 import RectCard from "../card/RectCard";
 
@@ -12,26 +16,61 @@ interface Props {
 const RectMediaScroll = ({ media, loading }: Props) => {
   const skeletons = Array.from({ length: 20 });
 
+  const prevRef = useRef<HTMLButtonElement | null>(null);
+  const nextRef = useRef<HTMLButtonElement | null>(null);
+
   return (
-    <>
+    //TODO: extract arrows and don't use tailwind classes here
+    <div className="group relative overflow-hidden">
+      {/* LEFT ARROW */}
+      <button
+        ref={prevRef}
+        className="absolute left-0 top-1/2 z-10 -translate-y-1/2 flex items-center justify-center
+                   opacity-0 group-hover:opacity-100 w-8
+                   transition-all duration-300
+                   h-full px-4
+                   bg-gradient-to-r from-black/70 to-transparent
+                   text-white text-3xl hover:scale-125 hover:bg-black/70! will-change-transform"
+      >
+        <FaChevronLeft size={20} />
+      </button>
+
+      {/* RIGHT ARROW */}
+      <button
+        ref={nextRef}
+        className="absolute right-0 top-1/2 z-10 -translate-y-1/2 flex items-center justify-center
+                   opacity-0 group-hover:opacity-100 w-8
+                   transition-all duration-300
+                   h-full px-4 
+                   bg-gradient-to-l from-black/70 to-transparent
+                   text-white text-3xl hover:scale-125 hover:bg-black/70! will-change-transform"
+      >
+        <FaChevronRight size={20} />
+      </button>
+
       <Swiper
         breakpoints={{
-          50: { slidesPerView: 2 },
-          375: { slidesPerView: 2 },
-          768: { slidesPerView: 3 },
-          1024: { slidesPerView: 4 },
+          50: { slidesPerView: 2.1 },
+          768: { slidesPerView: 3.1 },
+          1024: { slidesPerView: 4.1 },
         }}
-        loop={true}
         spaceBetween={10}
+        slidesPerGroup={2}
+        modules={[Navigation]}
+        navigation={{
+          nextEl: nextRef.current,
+          prevEl: prevRef.current,
+        }}
       >
         {loading &&
           skeletons.map((_, index) => (
-            <SwiperSlide key={index} style={{ display: loading ? "block" : "none" }}>
+            <SwiperSlide key={index}>
               <Skeleton>
                 <Box aspectRatio={16 / 9} />
               </Skeleton>
             </SwiperSlide>
           ))}
+
         {!loading &&
           media &&
           media.map((m) => (
@@ -40,7 +79,7 @@ const RectMediaScroll = ({ media, loading }: Props) => {
             </SwiperSlide>
           ))}
       </Swiper>
-    </>
+    </div>
   );
 };
 
