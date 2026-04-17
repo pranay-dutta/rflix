629a8fd1249d79aa811e967e977ee7ea08e17240
Pranay Dutta <rick15176@gmail.com>
2026-04-17 16:01:11 +0530

    feat: implement mute and unmute button

--- src/components/card/RectCard.tsx
+++ src/components/card/RectCard.tsx
@@ -3,13 +3,23 @@ import useTrailer from "@/hooks/useTrailer";
 import { Movie } from "@/interfaces/Movie";
 import TvSeries from "@/interfaces/TvSeries";
 import isMovie from "@/utils/isMovie";
-import { Box, HStack, Image, Progress, Skeleton, Text } from "@chakra-ui/react";
-import { useState } from "react";
+import {
+  Blockquote,
+  Box,
+  Button,
+  HStack,
+  Image,
+  Progress,
+  Skeleton,
+  Text,
+} from "@chakra-ui/react";
+import { MouseEvent, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { FaStar } from "react-icons/fa6";
 import { useRef } from "react";
 import useCustomizationStore from "@/store/customizationStore";
 import getPosterURL from "@/utils/getPosterURL";
+import { GoMute, GoUnmute } from "react-icons/go";
 
 interface Props {
   media: Movie | TvSeries;
@@ -37,7 +47,6 @@ const RectCard = ({ media }: Props) => {
   const imdbId = posterAndExternalIds?.external_ids.imdb_id;
   const posterPath = getPosterURL(isLoading, posterAndExternalIds);
 
-  //Video Trailer
   const {
     data: trailerURL,
     isFetching: trailerFetching,
@@ -45,23 +54,56 @@ const RectCard = ({ media }: Props) => {
   } = useTrailer(mediaType, isPreviewActive, imdbId);
 
   const navigate = useNavigate();
+  const titleTimer = useRef<ReturnType<typeof setTimeout> | null>(null);
   const hoverTimer = useRef<ReturnType<typeof setTimeout> | null>(null);
 
+  const [showTitle, setShowTitle] = useState(false);
+
+  const showPoster = !isPreviewActive || trailerFetching || !trailerURL;
+  const showBadges = fetchStatus === "idle" && !isPreviewActive;
+  const showVideo = isPreviewActive && trailerURL;
+
+  // When the mouse enters after 1 second show the preview of video
   const handlePointerEnter = () => {
     hoverTimer.current = setTimeout(() => {
       setIsPreviewActive(true);
     }, 1000);
   };
+
+  //title shown timer handler when hovering the card, it will show after 0.5s
+  if (showVideo) {
+    titleTimer.current = setTimeout(() => {
+      setShowTitle(true);
+    }, 700);
+  }
+
+  // When the mouse leaves clear the timer and hide the preview of video
   const handlePointerLeave = () => {
     if (hoverTimer.current) {
       clearTimeout(hoverTimer.current);
       hoverTimer.current = null;
     }
+
+    //clear title timer and hide title when mouse leaves
+    if (titleTimer.current) {
+      clearTimeout(titleTimer.current);
+      titleTimer.current = null;
+    }
+
+    //hide title immediately when mouse leaves
+    setShowTitle(false);
+
+    //show video preview when mouse leaves
     setIsPreviewActive(false);
   };
-  const showPoster = !isPreviewActive || trailerFetching || !trailerURL;
-  const showBadges = fetchStatus === "idle" && !isPreviewActive;
-  const showVideo = isPreviewActive && trailerURL;
+
+  const [isMuted, setMuted] = useState(true);
+
+  // Mute and unmute handler
+  const toggleMute = (e: MouseEvent<HTMLButtonElement>) => {
+    e.stopPropagation(); // Prevent the click from propagating to the card
+    setMuted((prev) => !prev);
+  };
 
   return (
     <Skeleton
@@ -79,6 +121,7 @@ const RectCard = ({ media }: Props) => {
         onClick={() => navigate(`/info/${mediaType}/${media.id}`)}
         overflow="hidden"
       >
+        {/* image poster of the media */}
         {showPoster && (
           <Image
             onLoad={() => setImgLoading(false)}
@@ -95,24 +138,64 @@ const RectCard = ({ media }: Props) => {
 
         {/* Trailer while hovering */}
         {showVideo && (
-            <video
-              src={trailerURL}
-              muted
-              loop
-              autoPlay
-              playsInline
-              style={{
-                position: "absolute",
-                inset: 0,
-                width: "100%",
-                height: "100%",
-                objectFit: "cover",
-                objectPosition: "center center",
-                display: "block",
-              }}
-            />
+          <video
+            src={trailerURL}
+            muted={isMuted}
+            loop
+            autoPlay
+            playsInline
+            style={{
+              position: "absolute",
+              inset: 0,
+              width: "100%",
+              height: "100%",
+              objectFit: "cover",
+              objectPosition: "center center",
+              display: "block",
+            }}
+          />
+        )}
+        {/* Mute/Unmute Button when video trailer is playing */}
+        {showVideo && (
+          <Button
+            top={2}
+            right={1}
+            {...badgeStyles}
+            borderRadius="full"
+            onClick={(e) => toggleMute(e)}
+            size="xs"
+          >
+            {isMuted ? <GoMute /> : <GoUnmute />}
+          </Button>
         )}
 
+        {/* Title flowing from left bottom */}
+        <Box
+          position="absolute"
+          bottom={4}
+          left={showVideo ? 0 : -20}
+          px={2}
+          py={1}
+          bg="blackAlpha.600"
+          opacity={showVideo ? 1 : 0}
+          transition="all 0.4s cubic-bezier(0.4, 0, 0.2, 1)"
+          borderRadius="sm"
+        >
+          <Blockquote.Root colorPalette={activePalette} variant="solid">
+            <Blockquote.Content>
+              <Text
+                fontSize="md"
+                fontWeight="semibold"
+                opacity={showTitle ? 1 : 0}
+                transition="all 0.3s ease-in-out"
+              >
+                {isMovie(media) ? media.title : media.name}
+              </Text>
+            </Blockquote.Content>
+          </Blockquote.Root>
+        </Box>
+
+        {/* Loading progress bar when video is fetching */}
         {isPreviewActive && trailerFetching && (
           <Progress.Root
             value={null}
