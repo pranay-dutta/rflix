c46a54983cc0564aad3172637bbd0cf84452411f
Pranay Dutta <rick15176@gmail.com>
2026-05-08 13:50:58 +0530

    update: route config

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,29 +1,28 @@
 import { createBrowserRouter } from "react-router-dom";
-import Layout from "./pages/Layout";
-import HomePage from "./pages/HomePage";
-import SearchPage from "./pages/SearchPage";
-import MovieInfoPage from "./pages/MovieInfoPage";
-import TvSeriesInfoPage from "./pages/TvSeriesInfoPage";
-import MovieWatchPage from "./pages/MovieWatchPage";
-import TvSeriesWatchPage from "./pages/TVSeriesWatchPage";
-import MovieGrid from "./components/MovieGrid";
 import AboutPage from "./pages/AboutPage";
-import TvSeriesGrid from "./components/TvSeriesGrid";
-import MovieDiscoverPage from "./pages/MovieDiscoverPage";
-import TvDiscoverPage from "./pages/TvDiscoverPage";
-import CustomizePage from "./pages/CustomizePage";
-import WatchListPage from "./pages/WatchListPage";
 import CastPageContainer from "./pages/cast/container/CastPageContainer";
+import CustomizePage from "./pages/CustomizePage";
+import DiscoverPage from "./pages/discover/DiscoverPage";
+import HomePage from "./pages/HomePage";
+import Layout from "./pages/Layout";
+import MovieInfoPage from "./pages/MovieInfoPage";
+import MoviePage from "./pages/MoviePage";
+import MovieWatchPage from "./pages/MovieWatchPage";
+import SearchPage from "./pages/SearchPage";
+import TVPage from "./pages/TVPage";
+import TvSeriesInfoPage from "./pages/TvSeriesInfoPage";
+import TvSeriesWatchPage from "./pages/TVSeriesWatchPage";
+import WatchListPage from "./pages/WatchListPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <HomePage />,
-  },
-  {
-    path: "/",
     element: <Layout />,
     children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
       {
         path: "info/movie/:id",
         element: <MovieInfoPage />,
@@ -45,20 +44,16 @@ const router = createBrowserRouter([
         element: <SearchPage />,
       },
       {
-        path: "movies/:tag",
-        element: <MovieGrid />,
+        path: "movies",
+        element: <MoviePage />,
       },
       {
-        path: "tvshows/:tag",
-        element: <TvSeriesGrid />,
+        path: "tv",
+        element: <TVPage />,
       },
       {
-        path: "discover/movie",
-        element: <MovieDiscoverPage />,
-      },
-      {
-        path: "discover/tv",
-        element: <TvDiscoverPage />,
+        path: "discover",
+        element: <DiscoverPage />,
       },
       {
         path: "about",
