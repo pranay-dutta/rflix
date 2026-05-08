c73b896b7f0d754280d710fd4470ac1927693fd6
Pranay Dutta <rick15176@gmail.com>
2026-05-08 15:18:17 +0530

    refactor: better drop down label for easy understanding

--- src/components/common/SortDropDown.tsx
+++ src/components/common/SortDropDown.tsx
@@ -3,78 +3,71 @@ import { useSearchParams } from "react-router-dom";
 import { Fragment } from "react/jsx-runtime";
 
 const SortDropDown = ({ mediaType }: { mediaType: "movie" | "tv" }) => {
-  return <DropDown mediaType={mediaType} />
-}
-
-const DropDown = ({ mediaType }: { mediaType: "movie" | "tv" }) => {
   const [, setSearchParams] = useSearchParams();
-  const collections = mediaType === "movie" ? movieSortCollections : tvSortCollections;
+  const collections =
+    mediaType === "movie" ? movieSortCollections : tvSortCollections;
 
   return (
-    <>
-      <Select.Root collection={collections} minW={{ base: "100%", md: "250px" }}>
-        <Select.HiddenSelect />
-        <Select.Control>
-          <Select.Trigger>
-            <Select.ValueText color="gray.300" placeholder="Select sort order" />
-          </Select.Trigger>
-          <Select.IndicatorGroup>
-            <Select.Indicator />
-          </Select.IndicatorGroup>
-        </Select.Control>
+    <Select.Root collection={collections}>
+      <Select.HiddenSelect />
+      <Select.Control>
+        <Select.Trigger>
+          <Select.ValueText placeholder="Select sort order" />
+        </Select.Trigger>
+        <Select.IndicatorGroup>
+          <Select.Indicator />
+        </Select.IndicatorGroup>
+      </Select.Control>
 
-        <Portal>
-          <Select.Positioner>
-            <Select.Content>
-              {collections.items.map((sortItem) => (
-                <Fragment key={sortItem.value} >
-                  <Select.Item
-                    item={sortItem}
-                    onClick={() => setSearchParams({ "sort_by": sortItem.value })}
-                  >
-                    {sortItem.label}
-                    <Select.ItemIndicator />
-                  </Select.Item>
-                </Fragment>
-              ))}
-            </Select.Content>
-          </Select.Positioner>
-        </Portal>
-      </Select.Root>
-    </>
+      <Portal>
+        <Select.Positioner>
+          <Select.Content>
+            {collections.items.map((sortItem) => (
+              <Fragment key={sortItem.value}>
+                <Select.Item
+                  item={sortItem}
+                  onClick={() => setSearchParams({ sort_by: sortItem.value })}
+                >
+                  {sortItem.label}
+                  <Select.ItemIndicator />
+                </Select.Item>
+              </Fragment>
+            ))}
+          </Select.Content>
+        </Select.Positioner>
+      </Portal>
+    </Select.Root>
   );
 };
 
 const movieSortCollections = createListCollection({
   items: [
-    { label: "Popularity Ascending", value: "popularity.asc" },
-    { label: "Popularity Descending", value: "popularity.desc" },
-    { label: "Vote Ascending", value: "vote_count.asc" },
-    { label: "Vote Descending", value: "vote_count.desc" },
-    { label: "Rating Ascending", value: "vote_average.asc" },
-    { label: "Rating Descending", value: "vote_average.desc" },
-    { label: "Release Date Ascending", value: "primary_release_date.asc" },
-    { label: "Release Date Descending", value: "primary_release_date.desc" },
+    { label: "High Votes", value: "vote_count.desc" },
+    { label: "Low Votes", value: "vote_count.asc" },
+    { label: "High Rating", value: "vote_average.desc" },
+    { label: "Low Rating", value: "vote_average.asc" },
+    { label: "More Popular", value: "popularity.desc" },
+    { label: "Less Popular", value: "popularity.asc" },
     { label: "Name (A-Z)", value: "original_title.asc" },
     { label: "Name (Z-A)", value: "original_title.desc" },
+    { label: "Latest", value: "primary_release_date.desc" },
+    { label: "Oldest", value: "primary_release_date.asc" },
   ],
 });
 
 const tvSortCollections = createListCollection({
   items: [
-    { label: "Popularity Ascending", value: "popularity.asc" },
-    { label: "Popularity Descending", value: "popularity.desc" },
-    { label: "Vote Ascending", value: "vote_count.asc" },
-    { label: "Vote Descending", value: "vote_count.desc" },
-    { label: "Rating Ascending", value: "vote_average.asc" },
-    { label: "Rating Descending", value: "vote_average.desc" },
-    { label: "First Air Date Ascending", value: "first_air_date.asc" },
-    { label: "First Air Date Descending", value: "first_air_date.desc" },
+    { label: "High Votes", value: "vote_count.desc" },
+    { label: "Low Votes", value: "vote_count.asc" },
+    { label: "High Rating", value: "vote_average.desc" },
+    { label: "Low Rating", value: "vote_average.asc" },
+    { label: "More Popular", value: "popularity.desc" },
+    { label: "Less Popular", value: "popularity.asc" },
     { label: "Name (A-Z)", value: "original_name.asc" },
     { label: "Name (Z-A)", value: "original_name.desc" },
+    { label: "Latest", value: "first_air_date.desc" },
+    { label: "Oldest", value: "first_air_date.asc" },
   ],
 });
 
-
-
-export default SortDropDown
+export default SortDropDown;
