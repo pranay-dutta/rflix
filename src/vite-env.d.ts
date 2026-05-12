2c471f96153c88fb3cc2563df60c717fd3190ad6
Pranay Dutta <rick15176@gmail.com>
2026-05-12 18:45:42 +0530

    refactor: update env example

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,11 +1,9 @@
 /// <reference types="vite/client" />
 interface ImportMetaEnv {
   VITE_TMDB_API_KEY: string;
-  VITE_TOGETHER_API_KEY: string;
-  VITE_BACKEND_CLIENT: string;
   VITE_DEV_BACKEND_CLIENT: string;
+  VITE_PROD_BACKEND_CLIENT: string;
   VITE_CLERK_PUBLISHABLE_KEY: string;
-  VITE_USE_TMDB_CLIENT: boolean;
 }
 
 interface ImportMeta {
